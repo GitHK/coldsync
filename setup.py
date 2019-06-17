c44d57131f92ee558818f8f94feea770de008a64
Andrei Neagu <neaguandrei@telematicainformatica.it>
2019-06-17 09:38:10 +0200

    bumped up version which also read file contents

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='coldsync',
-    version='0.0.1',
+    version='0.0.2',
     description='Used to upload file sto cold storage',
     author='Andrei Neagu',
     author_email='it.neagu.andrei@gmail.com',
